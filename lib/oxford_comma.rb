e301b9fef91654efa67f8b234bacd268d3080bcb
Nick Hall <nickhall122@gmail.com>
2017-09-07 18:56:57 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+
+  elsif array.length > 2
+  my_index = array.length-2
+  additional = ", and "+array[array.length-1]
+  newarray = array[0..my_index].join(", ")
+  newarray << additional
+end
 end
